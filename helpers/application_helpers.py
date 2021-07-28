bab550da07f74dfe45d74e6b30fc3353e4078776
hikmetyavuz <hikmet.yavuz@misli.com>
2021-07-28 21:37:36 +0300

    get_working_directory() was written.

--- helpers/application_helpers.py
+++ helpers/application_helpers.py
@@ -1,5 +1,6 @@
 import random
 import string
+import os
 
 
 class BackendHelper:
@@ -7,3 +8,8 @@ class BackendHelper:
     def create_random_aplhanumeric_string():
         token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(18))
         return token
+
+
+def get_working_directory():
+    working_directory = os.getcwd()
+    return working_directory
