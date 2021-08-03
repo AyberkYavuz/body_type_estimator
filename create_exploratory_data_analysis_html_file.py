9ddafe39ac76e66ee2f136d7f666918624d9e7d9
hikmetyavuz <hikmet.yavuz@misli.com>
2021-08-03 14:35:48 +0300

    create_exploratory_data_analysis_html_file.py was updated.

--- create_exploratory_data_analysis_html_file.py
+++ create_exploratory_data_analysis_html_file.py
@@ -1,4 +1,4 @@
-import pandas as pd
+from helpers.pandas_dataframe_helpers import get_data
 import pandas_profiling
 from helpers.application_helpers import get_working_directory
 from helpers.application_helpers import get_now
@@ -8,7 +8,7 @@ print("getting data.")
 working_directory = get_working_directory()
 data_path = working_directory + "/machine_learning_data/ObesityDataSet_raw_and_data_sinthetic.csv"
 print("Data path: " + data_path)
-data = pd.read_csv(data_path)
+data = get_data(data_path)
 
 print("creating exploratory_data_analysis.html file.")
 profile = pandas_profiling.ProfileReport(data, title="Pandas Profiling Report", explorative=True)
