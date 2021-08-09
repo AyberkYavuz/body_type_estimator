d525cb1dfb34986cfb20ec7d451faa85e2166f7f
hikmetyavuz <hikmet.yavuz@misli.com>
2021-08-09 18:03:18 +0300

    MatplotlibVisualizer, docstring was added.

--- core/visualizer.py
+++ core/visualizer.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 
 class MatplotlibVisualizer:
-    def show_horizontal_bar_plot(self, group_names, values, title_text, group_name_text, value_text):
-        plt.barh(group_names, values)
-        plt.title(title_text)
-        plt.ylabel(group_name_text)
-        plt.xlabel(value_text)
-        plt.show()
-
+    """Container of matplotlib methods.
+    """
     def show_feature_importances(self, importances, feature_names, top_n, font_size):
+        """Visualizes tree based algorithms feature importances.
+        Args:
+            importances: numpy array or list. Feature importances of tree based machine learning model.
+            feature_names: list. Names of important features
+            top_n: int. Feature set size.
+            font_size: int. Label font sizes of the plot.
+        """
         plt.rcParams.update({'font.size': font_size})
         feature_importances = pd.Series(importances, index=feature_names)
         feature_importances.nlargest(top_n).plot(kind='barh')
